2e993fdbb0c824d31eb5265e831841f79bcb58e6
Kayon Marie Douglas <kayonmarie.douglas@mymona.uwi.edu>
2019-04-17 15:05:19 +0000

    Edit to initial code

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,18 @@
 from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "v\xf9\xf7\x11\x13\x18\xfaMYp\xed_\xe8\xc9w\x06\x8e\xf0f\xd2\xba\xfd\x8c\xda"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://admin:password123@localhost/photogram"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
+
+db = SQLAlchemy(app)
+
+# Flask-Login login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+app.config.from_object(__name__)
 from app import views
